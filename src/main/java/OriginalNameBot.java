e1ad9e8e41a020b477e48ed63a9185d2b1173263
Keiran <keinn2210@gmail.com>
2026-01-19 16:48:52 +0800

    Add greeting

--- src/main/java/OriginalNameBot.java
+++ src/main/java/OriginalNameBot.java
@@ -1,10 +1,7 @@
 public class OriginalNameBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from original name bot! As you can see, this is a very original name. \n");
+        System.out.println("What can I do for you?");
+        System.out.println("Just kidding. I don't want to do anything for you. Bye.");
     }
 }
