e29b20d7ba80991a3590a1ae7cb527d17bd0cd16
Keiran <keinn2210@gmail.com>
2026-01-19 19:52:39 +0800

    Add support for echo

--- src/main/java/OriginalNameBot.java
+++ src/main/java/OriginalNameBot.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class OriginalNameBot {
     public static void main(String[] args) {
         System.out.println("Hello from original name bot! As you can see, this is a very original name. \n");
         System.out.println("What can I do for you?");
-        System.out.println("Just kidding. I don't want to do anything for you. Bye.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) break;
+            System.out.println(input);
+        }
+
+        System.out.println("Whew, finally. Bye.");
+        scanner.close();
     }
 }
