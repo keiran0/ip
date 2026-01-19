4266a0a9f26f01c47a8df0003401e65a94b213c6
Keiran <keinn2210@gmail.com>
2026-01-19 20:21:03 +0800

    Add add and list functionality

--- src/main/java/OriginalNameBot.java
+++ src/main/java/OriginalNameBot.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OriginalNameBot {
+
+    public static List<Task> tasks = new ArrayList<>();
+    public static int numTasks = 0;
+
     public static void main(String[] args) {
         System.out.println("Hello from original name bot! As you can see, this is a very original name. \n");
         System.out.println("What can I do for you?");
@@ -9,8 +15,19 @@ public class OriginalNameBot {
 
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) break;
-            System.out.println(input);
+            
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < numTasks; i++) {
+                    System.out.println(i + ". " + tasks.get(i).toString());
+                }
+            } else {
+                Task newTask = new Task(input);
+                tasks.add(newTask);
+                numTasks++;
+                System.out.println("added: " + newTask.toString());
+            }
         }
 
         System.out.println("Whew, finally. Bye.");
