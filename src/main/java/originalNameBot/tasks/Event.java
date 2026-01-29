7cb33bd359980806f24f5561ab3ccffd7ff6b6e6
Keiran <keinn2210@gmail.com>
2026-01-29 13:19:46 +0800

    Add whitespace to print statement

--- src/main/java/originalNameBot/tasks/Event.java
+++ src/main/java/originalNameBot/tasks/Event.java
@@ -14,6 +14,6 @@ public class Event extends Task {
     }
 
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + this.from + " to: " + this.to + ")";
+        return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
     }
 }
