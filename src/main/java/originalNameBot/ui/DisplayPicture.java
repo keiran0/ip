b2f927b704ce59bc805703ad21980fbe3ef804b3
Keiran <keinn2210@gmail.com>
2026-02-14 12:29:28 +0800

    Change DisplayPicture to ImageView to prevent component overlap

--- src/main/java/originalNameBot/ui/DisplayPicture.java
+++ src/main/java/originalNameBot/ui/DisplayPicture.java
@@ -2,26 +2,21 @@ package originalnamebot.ui;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
 
 /**
- * DisplayPicture is a wrapper around an ImageView and helps standardise the image format.
+ * DisplayPicture is an ImageView and helps standardise the image format.
  */
-public class DisplayPicture extends Region {
-
-    private ImageView displayPicture;
-    private Image image;
+public class DisplayPicture extends ImageView {
 
     /**
      * Creates a DisplayPicture of 100x100 size using the image in the filepath.
      * @param filepath Image filepath.
      */
     public DisplayPicture(String filepath) {
-        image = new Image(getClass().getResourceAsStream(filepath));
-        displayPicture = new ImageView(image);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-        getChildren().add(displayPicture);
+        Image image = new Image(getClass().getResourceAsStream(filepath));
+        setImage(image);
+        setFitWidth(100.0);
+        setFitHeight(100.0);
     }
 
 }
